1dced06351fb0d34b6c80efa635b411c0c4694b7
carolfeldhaus <carolinefe@hotmail.com>
2021-10-07 22:11:49 -0300

    adicionando os primeiros passos do app

--- Contador de Água/ViewController.swift
+++ Contador de Água/ViewController.swift
@@ -1,19 +1,55 @@
 //
 //  ViewController.swift
-//  Contador de Água
+//  App Aula Pedro
 //
 //  Created by Caroline Feldhaus de Souza on 07/10/21.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
+    class ViewController: UIViewController {
+            
+    @IBOutlet weak var qtdeagua: UILabel!
+    @IBOutlet weak var imagemgarrafa: UIImageView!
+    @IBOutlet weak var labelmeta: UILabel!
+    @IBOutlet weak var addbutt: UIButton!
+    @IBOutlet weak var removebutt: UIButton!
+    @IBOutlet weak var digitameta: UITextField!
+            
+    var contadorCopos: Int = 0
+            
+        override func viewDidLoad() {
+            super.viewDidLoad()
+                // Do any additional setup after loading the view.
+                
+                
+                digitameta.addTarget(self, action: #selector(atualizaMeta), for: .editingChanged)
+            }
+            
+    @objc func atualizaMeta(){
+                
+        guard let textoDoUsuario = digitameta.text else {
+            return
+                }
+                
+                labelmeta.text = "Minha meta: " + textoDoUsuario
+            }
+            
+    @IBAction func addbutt(_ sender: UIButton) {
+                contadorCopos += 1
+                //print(contadorCopos)
+                qtdeagua.text = String(contadorCopos) + " Copos"
+            }
+            
+    @IBAction func removebutt(_ sender: UIButton) {
+            if (contadorCopos > 0)
+                {
+                    contadorCopos -= 1
+                   // print(contadorCopos)
+                    qtdeagua.text = String(contadorCopos) + " Copos"
+                }
+        }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
+            
+        }
