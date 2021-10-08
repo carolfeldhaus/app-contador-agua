e6af2dfee4b8726d7752a9c0df73ee5c2aa9f3b1
carolfeldhaus <carolinefe@hotmail.com>
2021-10-07 21:08:45 -0300

    Initial Commit

---
+++ Contador de Água/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contador de Água
+//
+//  Created by Caroline Feldhaus de Souza on 07/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Contador de ÁguaTests/Contador_de_A_guaTests.swift
@@ -0,0 +1,33 @@
+//
+//  Contador_de_A_guaTests.swift
+//  Contador de ÁguaTests
+//
+//  Created by Caroline Feldhaus de Souza on 07/10/21.
+//
+
+import XCTest
+@testable import Contador_de_A_gua
+
+class Contador_de_A_guaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
